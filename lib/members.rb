0da32ea9668eab637e85eded8c364f774ad12170
kiburei <akiburei@gmail.com>
2017-07-07 17:19:23 +0300

    pass spec to reade object attributes

--- lib/members.rb
+++ lib/members.rb
@@ -1,4 +1,6 @@
 class Member
+  attr_reader(:full_name, :skill)
+
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:full_name)
     @skill = attributes.fetch(:skill)
--- spec/members_spec.rb
+++ spec/members_spec.rb
@@ -7,9 +7,16 @@ describe(Member) do
   end
 end
 
-describe('initialize') do
+describe('#initialize') do
   it('creates new member object') do
     member = Member.new({:full_name => "Minji Minji", :skill => "curving sponsors"})
     expect(member).to(eq(member))
+  end
+end
+
+describe('#attr_reader') do
+  it('access attribute of object using atribute reader') do
+      member = Member.new({:full_name => "Minji Minji", :skill => "curving sponsors"})
+      expect(member.skill).to(eq('curving sponsors'))
   end
 end
