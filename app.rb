b8e0ec9842366e40483d7d0d0035d52a73f63307
kiburei <akiburei@gmail.com>
2017-07-08 14:02:21 +0300

    add members to team

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ require('sinatra')
   post('/team') do
     name = params.fetch('team_name')
     project = params.fetch('project_name')
-    @team = Team.new({:team_name=> name, :project_name => project})
+    @team = Team.new({:team_name => name, :project_name => project})
     @team.save()
     erb(:success)
   end
@@ -26,6 +26,17 @@ require('sinatra')
     erb(:team)
   end
 
-  get('/members/new') do
+  get('/team/:id/members/new') do
+    @team = Team.find(params.fetch('id').to_i)
     erb(:member_form)
+  end
+
+  post('/team/:id/members') do
+    full_name = params.fetch('full_name')
+    skill = params.fetch('skill')
+    @member = Member.new({:full_name => full_name, :skill => skill })
+    @member.save
+    @team = Team.find(params.fetch('team_id'))
+    @team.add_member(@member)
+    erb(:team)
   end
